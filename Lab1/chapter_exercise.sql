3a389e471ffd3fe4afe77a804640d1ac29c27f49
大秦帝锅 <47657507+Quin293@users.noreply.github.com>
2024-10-24 22:14:43 +0800

    Update chapter_exercise.sql

--- Lab1/chapter_exercise.sql
+++ Lab1/chapter_exercise.sql
@@ -8,11 +8,9 @@ SELECT ID, name
 FROM instructor
 WHERE dept_name IN (SELECT dept_name FROM department WHERE building = 'Watson');
 
-SELECT DISTINCT student.ID, student.name
+SELECT DISTINCT ID, name
 FROM student
-JOIN takes ON student.ID = takes.ID
-JOIN course ON takes.course_id = course.course_id
-WHERE course.dept_name = 'Comp. Sci.';
+WHERE dept_name = 'Comp. Sci.';
 
 SELECT DISTINCT student.ID, student.name
 FROM student
